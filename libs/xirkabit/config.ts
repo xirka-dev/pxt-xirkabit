3dfda18a04e9d90a6cf553576babf1041d4a1018
Tengku Ahmad <tengku.ahmad@xooey.id>
2025-07-15 12:44:26 +0700

    Added xirka:bit board SVG and settings

--- libs/xirkabit/config.ts
+++ libs/xirkabit/config.ts
@@ -1,78 +1,27 @@
 namespace config {
-    export const NUM_NEOPIXELS = 1;
-
-    export const PIN_A0 = DAL.PA02;
-    export const PIN_A1 = DAL.PB08;
-    export const PIN_A2 = DAL.PB09;
-    export const PIN_A3 = DAL.PA04;
-    export const PIN_A4 = DAL.PA05;
-    export const PIN_A5 = DAL.PB02;
-
-    export const PIN_D0 = DAL.PA11;
-    export const PIN_D1 = DAL.PA10;
-    export const PIN_D2 = DAL.PA14;
-    export const PIN_D3 = DAL.PA09;
-    export const PIN_D4 = DAL.PA08;
-    export const PIN_D5 = DAL.PA15;
-    export const PIN_D6 = DAL.PA20;
-    export const PIN_D7 = DAL.PA21;
-    export const PIN_D8 = DAL.PA06;
-    export const PIN_D9 = DAL.PA07;
-    export const PIN_D10 = DAL.PA18;
-    export const PIN_D11 = DAL.PA16;
-    export const PIN_D12 = DAL.PA19;
-    export const PIN_D13 = DAL.PA17;
-    export const PIN_LED = PIN_D13;
-    export const PIN_RX = DAL.PA11;
-    export const PIN_TX = DAL.PA10;
-    export const PIN_MISO = DAL.PA12;
-    export const PIN_MOSI = DAL.PB10;
-    export const PIN_SCK = DAL.PB11;
-    export const PIN_SCL = DAL.PA23;
-    export const PIN_SDA = DAL.PA22;
-    export const PIN_RXLED = DAL.PB03;
-    export const PIN_TXLED = DAL.PA27;
-    export const PIN_NEOPIXEL = DAL.PA06;
-
-    export const PIN_FLASH_MISO = DAL.PA14;
-    export const PIN_FLASH_MOSI = DAL.PA08;
-    export const PIN_FLASH_SCK = DAL.PA09;
-    export const PIN_FLASH_CS = DAL.PA13;
-
-    export const PIN_ACCELEROMETER_INT = PIN_D5;
-
-    export const PIN_JACK_TX = PIN_TX;
-    export const PIN_JACK_COMMLED = PIN_TXLED;
-
-    export const PIN_DISPLAY_CS = PIN_A2;
-    export const PIN_DISPLAY_SCK = PIN_SCK;
-    export const PIN_DISPLAY_MOSI = PIN_MOSI;
-    export const PIN_DISPLAY_DC = PIN_A4;
-    export const PIN_DISPLAY_RST = PIN_A5;
-    export const PIN_DISPLAY_MISO = PIN_MISO;
-    // export const PIN_DISPLAY_BL = PIN_D4; plugged to power
-    export const DISPLAY_CFG0 = 0x00000090;
-    export const DISPLAY_CFG1 = 0x000e14ff;
-    export const DISPLAY_CFG2 = 24;
-    export const DISPLAY_WIDTH = 160;
-    export const DISPLAY_HEIGHT = 128;    
-}
-
-/*
-// M0
-namespace userconfig {
-    export const PIN_D2 = DAL.PA08;
-    export const PIN_D4 = DAL.PA14;
-    export const PIN_RXLED = DAL.PB03;
-    export const PIN_NEOPIXEL = -1;    
-    export const PIN_FLASH_SCK = -1;
     export const NUM_NEOPIXELS = 0;
+    
+    export const PIN_BTN_A = DAL.PA21;
+    export const PIN_BTN_B = DAL.PA16;
+    export const PIN_P20 = DAL.PA22; // PwmOnlyPin
+    export const PIN_P19 = DAL.PA23; // PwmOnlyPin
+    export const PIN_P16 = DAL.PA14; // PwmOnlyPin
+    export const PIN_P15 = DAL.PB10; // PwmOnlyPin
+    export const PIN_P14 = DAL.PA12; // PwmOnlyPin
+    export const PIN_P13 = DAL.PB11; // PwmOnlyPin
+    export const PIN_P12 = DAL.PA15; // PwmOnlyPin
+    export const PIN_P11 = DAL.PA16; // PwmOnlyPin
+    export const PIN_P10 = -1; // COLR5
+    export const PIN_P9 = DAL.PA17; // PwmOnlyPin
+    export const PIN_P8 = DAL.PA19; // PwmOnlyPin
+    export const PIN_P7 = -1; // COLR2
+    export const PIN_P6 = -1; // COLR4
+    export const PIN_P5 = DAL.PA21; // PwmOnlyPin
+    export const PIN_P4 = -1; //COLR1
+    export const PIN_P3 = -1; // COLR3
+    export const PIN_P2 = DAL.PA09; // PwmPin
+    export const PIN_P1 = DAL.PA11; // PwmPin
+    export const PIN_P0 = DAL.PA10; // PwmPin
+    
+    // additional pins to look for: SPI, I2C, Flash
 }
-// Zero
-namespace userconfig {
-    export const PIN_RXLED = DAL.PB03;
-    export const PIN_NEOPIXEL = -1;    
-    export const PIN_FLASH_SCK = -1;
-    export const NUM_NEOPIXELS = 0;
-}
-*/
