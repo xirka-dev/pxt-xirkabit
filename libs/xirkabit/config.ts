cc6d623203578b7c7068a1f811934278d5222cf5
Tengku Ahmad <tengku.ahmad@xooey.id>
2025-07-31 10:36:12 +0700

    Added SAMD pins definition into DAL
    for use in TypeScript;
    Corrected pin type on some pins.

--- libs/xirkabit/config.ts
+++ libs/xirkabit/config.ts
@@ -23,5 +23,46 @@ namespace config {
     export const PIN_P1 = DAL.PA11; // PwmPin
     export const PIN_P0 = DAL.PA10; // PwmPin
     
+    export const PIN_A0  = DAL.PA00;
+    export const PIN_A1  = DAL.PA01;
+    export const PIN_A2  = DAL.PA02;
+    export const PIN_A3  = DAL.PA03;
+    export const PIN_A4  = DAL.PA04;
+    export const PIN_A5  = DAL.PA05;
+    export const PIN_A6  = DAL.PA06;
+    export const PIN_A7  = DAL.PA07;
+    export const PIN_A8  = DAL.PA08;
+    export const PIN_A9  = DAL.PA09;
+    export const PIN_A10 = DAL.PA10;
+    export const PIN_A11 = DAL.PA11;
+    export const PIN_A12 = DAL.PA12;
+    export const PIN_A13 = DAL.PA13;
+    export const PIN_A14 = DAL.PA14;
+    export const PIN_A15 = DAL.PA15;
+    export const PIN_A16 = DAL.PA16;
+    export const PIN_A17 = DAL.PA17;
+    export const PIN_A18 = DAL.PA18;
+    export const PIN_A19 = DAL.PA19;
+    export const PIN_A20 = DAL.PA20;
+    export const PIN_A21 = DAL.PA21;
+    export const PIN_A22 = DAL.PA22;
+    export const PIN_A23 = DAL.PA23;
+    export const PIN_A24 = DAL.PA24;
+    export const PIN_A25 = DAL.PA25;
+    // export const PIN_A26 = DAL.PA26;
+    export const PIN_A27 = DAL.PA27;
+    export const PIN_A28 = DAL.PA28;
+    // export const PIN_A29 = DAL.PA29;
+    export const PIN_A30 = DAL.PA30;
+    export const PIN_A31 = DAL.PA31;
+
+    export const PIN_B2  = DAL.PB02;
+    export const PIN_B3  = DAL.PB03;
+    export const PIN_B8  = DAL.PB08;
+    export const PIN_B9  = DAL.PB09;
+    export const PIN_B10 = DAL.PB10;
+    export const PIN_B11 = DAL.PB11;
+    export const PIN_B22 = DAL.PB22;
+    export const PIN_B23 = DAL.PB23;
     // additional pins to look for: SPI, I2C, Flash
 }
--- libs/xirkabit/device.d.ts
+++ libs/xirkabit/device.d.ts
@@ -7,30 +7,30 @@ declare namespace pins {
     const P2: PwmPin;
     
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P5)
-    const P5: PwmPin;
+    const P5: PwmOnlyPin;
 
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P8)
-    const P8: PwmPin;
+    const P8: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P9)
-    const P9: PwmPin;
+    const P9: PwmOnlyPin;
     
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P11)
-    const P11: PwmPin;
+    const P11: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P12)
-    const P12: PwmPin;
+    const P12: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P13)
-    const P13: PwmPin;
+    const P13: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P14)
-    const P14: PwmPin;
+    const P14: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P15)
-    const P15: PwmPin;
+    const P15: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P16)
-    const P16: PwmPin;
+    const P16: PwmOnlyPin;
 
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P19)
-    const P19: PwmPin;
+    const P19: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_P20)
-    const P20: PwmPin;
+    const P20: PwmOnlyPin;
 
     //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_BTN_A)
     const BTN_A: DigitalInOutPin;
