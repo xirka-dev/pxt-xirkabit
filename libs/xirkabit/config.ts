c559fbfaa99920030d6552782a0ec4177aedca91
Tengku Ahmad <tengku.ahmad@xooey.id>
2025-09-04 12:54:52 +0700

    Replaced lib `mixer---samd` with `music---pwm`

--- libs/xirkabit/config.ts
+++ libs/xirkabit/config.ts
@@ -75,4 +75,6 @@ namespace config {
     export const PIN_SCK  = PIN_B11;
     export const PIN_MISO = PIN_A12;
     export const PIN_MOSI = PIN_B10;
+
+    export const PIN_SPEAKER_AMP = PIN_A18;
 }
