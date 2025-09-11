29be1b86006d0274abf9824b96a593d13b67f010
Tengku Ahmad <tengku.ahmad@xooey.id>
2025-09-11 17:56:57 +0700

    Added `play` methods for Music blocks
    by copying `playable.ts` from `pxt-common-packages/libs/mixer` into `ns.ts`;
    Added playable class for built-in melody;
    Corrected default parameter value of `sounds()` function;
    Added default PWM volume.

---
+++ libs/music---pwm/ns.ts
@@ -0,0 +1,195 @@
+
+/**
+ * Generation of music tones.
+ */
+//% color=#E63022 weight=106 icon="\uf025"
+//% groups='["Melody", "Tone", "Volume", "Tempo", "Melody Advanced"]'
+namespace music {
+
+    export enum PlaybackMode {
+        //% block="until done"
+        UntilDone,
+        //% block="in background"
+        InBackground,
+        //% block="looping in background"
+        LoopingInBackground
+    }
+
+    let looping: Playable[];
+
+    export class Playable {
+        stopped: boolean;
+        constructor() {
+
+        }
+
+        _play(playbackMode: PlaybackMode) {
+            // subclass
+        }
+
+        loop() {
+            if (!looping) {
+                looping = [];
+            }
+
+            looping.push(this);
+            this.stopped = false;
+
+            control.runInParallel(() => {
+                while (!this.stopped) {
+                    this._play(PlaybackMode.UntilDone);
+                }
+            });
+        }
+    }
+
+    // export class StringArrayPlayable extends Playable {
+    //     constructor(private notes: string[], private tempo: number) {
+    //         super();
+    //     }
+
+    //     _play(playbackMode: PlaybackMode) {
+    //         if(this.tempo) {
+    //             music.setTempo(this.tempo);
+    //         }
+    //         if (playbackMode == PlaybackMode.InBackground) {
+    //             startMelodyInternal(this.notes, MelodyOptions.OnceInBackground);
+    //         }
+    //         else if (playbackMode == PlaybackMode.LoopingInBackground) {
+    //             startMelodyInternal(this.notes, MelodyOptions.ForeverInBackground);
+    //         }
+    //         else {
+    //             startMelodyInternal(this.notes, MelodyOptions.Once);
+    //             waitForMelodyEnd();
+    //         }
+    //     }
+    // }
+
+    export class TonePlayable extends Playable {
+        constructor(public pitch: number, public duration: number) {
+            super();
+        }
+
+        _play(playbackMode: PlaybackMode) {
+            if (playbackMode === PlaybackMode.InBackground) {
+                control.runInParallel(() => music.playTone(this.pitch, this.duration));
+            }
+            else if (playbackMode === PlaybackMode.UntilDone) {
+                music.playTone(this.pitch, this.duration);
+            }
+            else {
+                this.loop();
+            }
+        }
+    }
+
+    export class BuiltInMelodyPlayable extends Playable {
+        constructor(public sound: string) {
+            super();
+        }
+
+        _play(playbackMode: PlaybackMode) {
+            if (playbackMode === PlaybackMode.InBackground) {
+                music.playSound(this.sound)
+            }
+            else if (playbackMode === PlaybackMode.UntilDone) {
+                music.playSoundUntilDone(this.sound)
+            }
+            else {
+                this.loop();
+            }
+        }
+    }
+
+    /**
+     * Play a song, melody, or other sound. The music plays until finished or can play as a
+     * background task.
+     * @param toPlay the song or melody to play
+     * @param playbackMode play the song or melody until it's finished or as background task
+     */
+    //% blockId="music_playable_play"
+    //% block="play $toPlay $playbackMode"
+    //% toPlay.shadow=music_string_playable
+    //% group="Melody"
+    //% help="music/play"
+    //% blockHidden
+    export function play(toPlay: Playable, playbackMode: PlaybackMode) {
+        toPlay._play(playbackMode);
+    }
+
+    //% blockId="music_playable_play_default_bkg"
+    //% block="play $toPlay $playbackMode"
+    //% toPlay.shadow=music_string_playable
+    //% playbackMode.defl=music.PlaybackMode.InBackground
+    //% group="Melody"
+    //% help="music/play"
+    //% blockHidden
+    export function _playDefaultBackground(toPlay: Playable, playbackMode: PlaybackMode) {
+        return play(toPlay, playbackMode);
+    }
+
+    /**
+     * Play a melody from the melody editor
+     * @param melody string of up to eight notes [C D E F G A B C5] or rests [-] separated by spaces, which will be played one at a time, ex: "E D G F B A C5 B "
+     * @param bpm number in beats per minute dictating how long each note will play
+     */
+    //% blockId="music_string_playable"
+    //% block="melody $melody at tempo $bpm|(bpm)"
+    //% weight=85 blockGap=8
+    //% help=music/string-playable
+    //% group="Melody"
+    //% toolboxParent=music_playable_play
+    //% toolboxParentArgument=toPlay
+    //% duplicateShadowOnDrag
+    //% melody.shadow=melody_editor
+    //% bpm.min=40 bpm.max=500
+    //% bpm.defl=120
+    // export function stringPlayable(melody: string, bpm: number): Playable {
+    //     return new StringArrayPlayable(music.getMelodyNotes(melody), bpm);
+    // }
+
+    /**
+     * Plays a tone through pin ``P0`` for the given duration.
+     * @param note pitch of the tone to play in Hertz (Hz).
+     * @param duration tone duration in milliseconds (ms)
+     */
+    //% blockId="music_tone_playable"
+    //% block="tone $note for $duration"
+    //% toolboxParent=music_playable_play
+    //% toolboxParentArgument=toPlay
+    //% group="Tone"
+    //% weight=85
+    //% duplicateShadowOnDrag
+    //% note.shadow=device_note
+    //% duration.shadow=device_beat
+    //% parts="headphone"
+    //% help=music/tone-playable
+    export function tonePlayable(note: number, duration: number): Playable {
+        return new TonePlayable(note, duration);
+    }
+
+    /**
+     * Play a built-in melody
+     * @param melody the melody to play
+     */
+    //% blockId="music_sound_playable"
+    //% block="melody $melody"
+    //% weight=60
+    //% group="Melody Advanced"
+    //% toolboxParent=music_playable_play_default_bkg
+    //% toolboxParentArgument=toPlay
+    //% duplicateShadowOnDrag
+    //% melody.shadow=music_sounds
+    export function builtInMelodyPlayable(melody: string): Playable {
+        return new BuiltInMelodyPlayable(melody);
+    }
+
+    export function _stopPlayables() {
+        if (!looping) return;
+
+        for (const p of looping) {
+            p.stopped = true;
+        }
+        looping = undefined;
+    }
+}
--- libs/xirkabit/config.ts
+++ libs/xirkabit/config.ts
@@ -77,4 +77,5 @@ namespace config {
     export const PIN_MOSI = PIN_B10;
 
     export const PIN_SPEAKER_AMP = PIN_A18;
+    export const SPEAKER_VOLUME = 512;
 }
