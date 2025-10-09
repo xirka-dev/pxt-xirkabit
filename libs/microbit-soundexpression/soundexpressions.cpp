75efd0fa9e1edd7112c59754cbcc51b189aa755e
Tengku Ahmad <tengku.ahmad@xooey.id>
2025-10-09 15:08:43 +0700

    Added SoundExpression library from micro:bit sources
    Not yet working, but using the blocks don't cause hang;
    Simulation still errors out;
    Updated `dal.d.ts` file and settings for the library.

---
+++ libs/microbit-soundexpression/soundexpressions.cpp
@@ -0,0 +1,22 @@
+#include "pxt.h"
+
+namespace music {
+    /**
+     * Internal use only
+     **/
+    //% async
+    void __playSoundExpression(String nodes, bool waitTillDone) {
+        // if (waitTillDone)
+        //     uBit.audio.soundExpressions.play(MSTR(nodes));
+        // else
+        //     uBit.audio.soundExpressions.playAsync(MSTR(nodes));
+    }
+
+    /**
+    * Internal use only
+    */
+    //% 
+    void __stopSoundExpressions() {
+        // uBit.audio.soundExpressions.stop();
+    }
+}
