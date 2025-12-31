a243be7d811c65c383090cf684d487f1bc6bfbee
GeorgeCZ <george.cyzys@gmail.com>
2025-12-31 14:23:19 +0700

    Adding lightlevel block

---
+++ libs/xirkabit/lightlevel.cpp
@@ -0,0 +1,18 @@
+#include "pxtAddon.h"
+
+namespace xirkabit {
+
+    //%
+    int lightLevelInternal() {
+        pxt::serialXirkabit::sendCommand("ALED", nullptr, false);
+
+        int lv = pxt::serialXirkabit::readResponse();
+        lv = lv / 4;
+
+        if (lv < 0) lv = 0;
+        if (lv > 255) lv = 255;
+
+        return lv;
+    }
+
+}
---
+++ libs/xirkabit/sim/lightlevel.ts
@@ -0,0 +1,6 @@
+namespace pxsim.xirkabit {
+    export function lightLevelInternal(): number {
+        // nilai dummy untuk simulator
+        return 100;
+    }
+}
