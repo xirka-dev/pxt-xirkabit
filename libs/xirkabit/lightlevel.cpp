fffb5c712e379f0696fbb5f052f6344a397591ed
GeorgeCZ <george.cyzys@gmail.com>
2026-01-07 16:03:35 +0700

    adding math

--- libs/xirkabit/lightlevel.cpp
+++ libs/xirkabit/lightlevel.cpp
@@ -1,4 +1,5 @@
 #include "pxtAddon.h"
+#include "math.h"
 
 namespace xirkabit {
 
@@ -6,12 +7,21 @@ namespace xirkabit {
     int lightLevelInternal() {
         pxt::serialXirkabit::sendCommand("ALED", nullptr, false);
 
-        int lv = pxt::serialXirkabit::readResponse();
-        //lv = lv / 4;
+        int raw = pxt::serialXirkabit::readResponse();
+        if (raw < 0) raw = 0;
+        if (raw > 255 ) raw = 255;
 
+        float norm = raw / 255.0f;
+        float gamma = sqrt(norm);
+
+        int lv = (int)(gamma * 255.0f);
+
+        if (lv > 255) lv = 255;
+        if (lv < 0) lv = 0;
+        /*
         if (lv < 0) lv = 0;
         if (lv > 255) lv = 255;
-
+        */
         return lv;
     }
 
