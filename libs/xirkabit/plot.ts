57f04f81ce2477e4a0edbdfab0c8963fff2cd9e1
GeorgeCZ <george.cyzys@gmail.com>
2026-03-03 15:30:07 +0700

    plot mask bug fix. plot bar bug fix.

--- libs/xirkabit/plot.ts
+++ libs/xirkabit/plot.ts
@@ -36,56 +36,14 @@ namespace led {
         return pointNative(x, y)
     }
 
-
-    // what's the current high value
-    let barGraphHigh = 0;
-    // when was the current high value recorded
-    let barGraphHighLast = 0;
-
-    /**
-     * Controls where plotbargraph prints to the console
-     **/
-    export let barGraphToConsole = true
-
-        /**
-     * Displays a vertical bar graph based on the `value` and `high` value.
-     * If `high` is 0, the chart gets adjusted automatically.
-     * @param value current value to plot
-     * @param high maximum value. If 0, maximum value adjusted automatically, eg: 0
-     * @param valueToConsole if true, prints value to the serial port
-     */
     //% help=led/plot-bar-graph weight=20
     //% blockId=device_plot_bar_graph block="plot bar graph of $value up to $high|| serial write $valueToConsole" icon="\uf080" blockExternalInputs=true
-    //% parts="ledmatrix"
+    //% parts="ledmatrix" shim=led::plotBarGraph
     //% valueToConsole.shadow=toggleOnOff
     //% valueToConsole.defl=true
     export function plotBarGraph(value: number, high: number): void
     {
-        if (isNaN(value)) {
-            basic.clearScreen()
-            return
-        }
-
-        value = Math.abs(value)
-
-        if (high <= 0) high = 1
-
-        let v = value / high
-        let dv = 1 / 16
-        let k = 0
-
-        for (let y = 4; y >= 0; --y) {
-            for (let x = 0; x < 3; ++x) {
-                if (k > v) {
-                    led.unplot(2 - x, y)
-                    led.unplot(2 + x, y)
-                } else {
-                    led.plot(2 - x, y)
-                    led.plot(2 + x, y)
-                }
-                k += dv
-            }
-        }
+        return
     }
 
     //% shim=led::plot
