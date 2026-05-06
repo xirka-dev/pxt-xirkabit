a9681d088ca4cfa5dd7f5ecd634dd9ccff39bb5d
GeorgeCZ <george.cyzys@gmail.com>
2026-05-06 15:46:12 +0700

    using global brightness

--- libs/xirkabit/images.cpp
+++ libs/xirkabit/images.cpp
@@ -151,15 +151,18 @@ void plotImage(BlocklyImage sprite, int xOffset = 0) {
         for(int j=0; j<5; j++)
             led::matrixState[i][j] = (image.data[i] & (1<<(4-j))) ? true : false; 
 
-    char jsonData[128];
-    snprintf(jsonData, 127, "[%u,%u,%u,%u,%u]",
-        image.data[0],
-        image.data[1],
-        image.data[2],
-        image.data[3],
-        image.data[4]
-    );
-    serialXirkabit::sendCommand("LMTRX", jsonData);
+    led::matrixDirty = true;
+    led::tickMatrix();
+
+    // char jsonData[128];
+    // snprintf(jsonData, 127, "[%u,%u,%u,%u,%u]",
+    //     image.data[0],
+    //     image.data[1],
+    //     image.data[2],
+    //     image.data[3],
+    //     image.data[4]
+    // );
+    // serialXirkabit::sendCommand("LMTRX", jsonData);
 }
 
 /**
